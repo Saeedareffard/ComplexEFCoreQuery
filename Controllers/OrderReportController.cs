ca7f728acc9d715a453fed597540c44ea8340986
s.areffard <saeedareffard@yahoo.com>
2023-12-06 16:09:06 +1300

    chore: create a separate controller for order report

---
+++ Controllers/OrderReportController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticeLinq.Dtos;
+using PracticeLinq.Interfaces;
+
+namespace PracticeLinq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderReportController : ControllerBase
+    {
+    private readonly IOrderReportService _service;
+
+    public OrderReportController(IOrderReportService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("GetReport")]
+        public  List<OrderReportDto> GetReport([FromQuery] int? year)
+        {
+            return  _service.GetOrdersReport(year);
+        }
+
+    }
+}
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,44 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
-using PracticeLinq.Dtos;
-using PracticeLinq.Interfaces;
-using PracticeLinq.Services;
 
-namespace PracticeLinq.Controllers
+namespace PracticeLinq.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries =
     {
-        private readonly IOrderReportService _service;
-        private static readonly string[] Summaries = new[]
-        {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOrderReportService service)
-        {
-            _logger = logger;
-            _service = service;
-        }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-        }
-
-        [HttpGet("GetReport")]
-        public  List<OrderReportDto> GetReport([FromQuery] int? year)
-        {
-            return  _service.GetOrdersReport(year);
-        }
     }
 }
